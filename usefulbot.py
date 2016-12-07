e45be6f32954186a6483c76d2b8c35715269a80e
spartanboy56 <brd4jc@gmail.com>
2016-12-06 21:31:02 -0500

    cleaned up and commented things

--- usefulbot.py
+++ usefulbot.py
@@ -1,26 +1,23 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
-from os.path import dirname
 from slackbot.bot import Bot
 import inspect
 import sys
 import re
-import random
 import pyowm
 
-#sys.path.append(dirname("/home/jbird/dicemaniac/"))
-# to get the slackbot_settings.py file that contains the API token
 
-# some default info
+
+# defaults
 DEFAULT_REPLY = "Sorry but I'm a complete wank"
-ERRORS_TO = 'jeremy'
+ERRORS_TO = 'chris'
 
 owm = pyowm.OWM('6a47e3b888d6f1962270697e72ed9f99')
 
 
-######
-# IMPORTANT MACROS
-###
+
+# functions for the @listeners below
+
 def tx(message):
     return message._body.get('text')
 
@@ -40,30 +37,33 @@ def getBotWeather(weather):
     humidity = weather.get_humidity()
     weatherstring = 'The weather is ' + status + ' with a temperature of ' + str(temp) + '°F' + ' and a humidity of ' + str(humidity) +'%'
     return weatherstring
-#####
-
-### custom plugins. let's see if this actually works.
 
 
-@listen_to('usefulbot help')
+#@listen_to('trigger word')
+#def functionName(message):
+#code triggered by word
+@listen_to('usefulbot  ' or 'usefulbot help')
 def usefulHelp(message):
     message.reply('Commands: weather [city] [state/country]')
 
+@listen_to('usefulbot github')
+def usefulGithub(message):
+    message.reply('https://github.com/spartanboy56/usefulbot')    
+
+#next on the todo list.
 @listen_to('relevant xkcd', re.IGNORECASE)
 def relevant(message):
-    message.reply('I no work yet!')
+    message.reply("I don't work yet. Someone build me")
 
 @listen_to('weather', re.IGNORECASE)
 def weather(message):
-    if(re.search('weather', tx(message))):
        location = getLocation(message)
        observation = owm.weather_at_place(location)
        w = observation.get_weather()
-       outputweather = getBotWeather(w)
-       message.reply(outputweather)
+       message.reply(getBotWeather(w))
 
    
-# Revamped roll function. Checking for + and - modifiers at the end. Maybe separate functions per scenario?
+
 
 
 # main loop
@@ -71,6 +71,6 @@ def main():
     bot = Bot()
     bot.run()
 
-# no idea why this is necesary but the docs said to put this here lol
+
 if __name__ == "__main__":
     main()
